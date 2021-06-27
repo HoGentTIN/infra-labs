1c6db652f00305a7234097ac2043946776418cb2
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2021-06-27 22:18:52 +0200

    Add sample app for the CI/CD assignment

---
+++ dockerlab/labs/cicd-sample-app/sample-app.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euo pipefail
+
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
+
+cp sample_app.py tempdir/.
+cp -r templates/* tempdir/templates/.
+cp -r static/* tempdir/static/.
+
+cat > tempdir/Dockerfile << _EOF_
+FROM python
+RUN pip install flask
+COPY  ./static /home/myapp/static/
+COPY  ./templates /home/myapp/templates/
+COPY  sample_app.py /home/myapp/
+EXPOSE 5050
+CMD python /home/myapp/sample_app.py
+_EOF_
+
+cd tempdir || exit
+docker build -t sampleapp .
+docker run -t -d -p 5050:5050 --name samplerunning sampleapp
+docker ps -a 
